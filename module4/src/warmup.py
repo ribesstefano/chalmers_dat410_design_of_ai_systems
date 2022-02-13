1db678e90f0e38f9a5e19da32351ffd53a54318e
danielemurgolo <98823551+danielemurgolo@users.noreply.github.com>
2022-02-13 16:47:48 +0100

    Update warmup.py

--- module4/src/warmup.py
+++ module4/src/warmup.py
@@ -1,1 +1,19 @@
 from collections import Counter
+import re
+
+def open_file(name):
+    f = open(name, "r")
+    text = f.read()
+    return text
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    deutsch_text = open_file("data/dat410_europarl/europarl-v7.de-en.lc.de")
+    deutsch_text = re.sub("[^\w ]", '', deutsch_text)
+    deutsch_words = deutsch_text.split(' ')
+    deutsch_words = filter(None, deutsch_words)
+    c = Counter(deutsch_words)
+    print(c.most_common(10))
