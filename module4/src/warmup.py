b867120d5a6c8f0e787de19705b80d155ce9ccc9
Stefano Ribes <stefanor@got.gaisler.com>
2022-02-14 17:30:33 +0100

    Module4: Cleaned warmup file

--- module4/src/warmup.py
+++ module4/src/warmup.py
@@ -2,25 +2,24 @@ from collections import Counter
 import re
 
 def open_file(name):
-    f = open(name, "r")
-    text = f.read()
+    with open(name, 'r') as f:
+        text = f.read()
     return text
 
 def clean_text(text):
     text = re.sub("[^\w ]", '',text)
     return text.split(' ')
 
-def count_words(word_list):
+def count_words(word_list, verbose=0):
     ''' counter '''
     c = Counter(word_list)
-    print(c.most_common(10))
+    if verbose:
+        print(c.most_common(10))
     return c
 
+def get_counter(filename):
+    return count_words(clean_text(open_file(filename)))
 
-
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     text = open_file("data/dat410_europarl/europarl-v7.de-en.lc.de")
     word_list = clean_text(text)
