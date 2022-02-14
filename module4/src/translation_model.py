80a96a6e492a1f43abfc61b102181cb9d227d53a
Stefano Ribes <stefanor@got.gaisler.com>
2022-02-14 17:42:02 +0100

    Module4: polished translation model and removed numpy dependency

--- module4/src/translation_model.py
+++ module4/src/translation_model.py
@@ -1,40 +1,42 @@
 from collections import defaultdict as ddict
-import numpy as np
-import matplotlib as mpl
-import pandas as pd
+# import matplotlib as mpl
+# import pandas as pd
 
-def train(eng,fr,prtable,debug=False):
+def train(eng, fr, prtable=None, debug=False):
+    if prtable is None:
+        prtable = ddict(lambda: 1e-6)
     ec_num = ddict(lambda:1e-6)
     ec_den = ddict(lambda:1e-6)
     # E step
     if debug:
         print('E Step')
-    for eSent,fSent in zip(eng,fr):
+    for eSent, fSent in zip(eng, fr):
         # split the words and insert NULL word
         e = eSent.strip().split(' ')
-        e.insert(0,'NULL')
+        e.insert(0, 'NULL')
         f = fSent.strip().split(' ')
         l = len(e)
         m = len(f)
         # Calculate the expected counts for E step
         for j in range(m):
-            a = np.zeros(l)
+            # a = np.zeros(l)
+            a = [0] * l
             for i in range(l):
-                a[i]=prtable[f[j],e[i]]
+                a[i] = prtable[f[j],e[i]]
                 if debug:
-                    print('Pr. Lookup:',e[i],f[j],prtable[f[j],e[i]],np.sum(a))
-            a = a/np.sum(a)
+                    print('Pr. Lookup:', e[i], f[j], prtable[f[j], e[i]], sum(a))
+            a = [x / sum(a) for x in a]
             for i in range(l):
-                ec_num[f[j],e[i]] += a[i]
-                ec_den[e[i]]+=a[i]
+                ec_num[f[j], e[i]] += a[i]
+                ec_den[e[i]] += a[i]
     # M Step
     if debug:
         print('M Step')
-    for fj,ei in prtable.keys():
+    for fj, ei in prtable.keys():
         # calculate p(fj|ei) table from expected counts
-        prtable[fj,ei] = ec_num[fj,ei]/ec_den[ei]
+        prtable[fj, ei] = ec_num[fj, ei]/ec_den[ei]
         if debug:
-            print(ei,fj,ec_num[fj,ei],prtable[fj,ei])
+            print(ei, fj, ec_num[fj, ei], prtable[fj, ei])
     return prtable
 
 def readfile(filename):
@@ -53,14 +55,12 @@ def get_prob(prtabel, fw, ew):
     else:
         return 0.00001
 
-
 def is_in_keys(prtabel, ew):
     keys = prtabel.keys()
     f_words = []
     for key in keys:
         if ew in key:
             f_words.append(key[0])
-
     return f_words
 
 def most_probable_words(prtable, ew, f_words, n):
@@ -86,17 +86,16 @@ def get_bigram_prob(prtabel, f_bigram, e_bigram):
 
     return get_prob(prtabel, f_bigram[0], e_bigram[0])*get_prob(prtabel, f_bigram[1], e_bigram[1])
 
-trainlist_eng = readfile('test.eng')
-trainlist_fra = readfile('test.fra')
-prtable = ddict(lambda: 1e-6)
-prtable = train(trainlist_eng,trainlist_fra,prtable)
+if __name__ == '__main__':
+    trainlist_eng = readfile('test.eng')
+    trainlist_fra = readfile('test.fra')
+    prtable = ddict(lambda: 1e-6)
+    prtable = train(trainlist_eng,trainlist_fra,prtable)
 
+    prob = get_bigram_prob(prtable, ['nature','cours'], ['nature', 'issues'])
 
+    f_words = is_in_keys(prtable, 'nature')
 
-prob = get_bigram_prob(prtable, ['nature','cours'], ['nature', 'issues'])
+    df = most_probable_words(prtable, 'european', f_words, 3)
 
-f_words = is_in_keys(prtable, 'nature')
-
-df = most_probable_words(prtable, 'european', f_words, 3)
-
-print(df)
+    print(df)
