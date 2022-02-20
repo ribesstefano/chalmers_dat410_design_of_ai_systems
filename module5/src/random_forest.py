1b5673a2b05cd879cfe7c243a51fb88b08804834
danielemurgolo <98823551+danielemurgolo@users.noreply.github.com>
2022-02-20 18:36:53 +0100

    file random_forest.py classifies the dataset using sklearn RandomForest classifier. It also provides a score of the model and the importance of each feature

---
+++ module5/src/random_forest.py
@@ -0,0 +1,42 @@
+from sklearn.ensemble import RandomForestClassifier
+import data_cleaner
+from sklearn import metrics
+import pandas as pd
+import pickle
+
+clf = RandomForestClassifier(n_estimators=10)
+
+def train_model(x_train, y_train):
+
+    clf.fit(x_train, y_train)
+
+    return clf
+
+def predict_model(x_test):
+
+    return clf.predict(x_test)
+
+def score_model(y_pred, y_test):
+    return metrics.accuracy_score(y_test, y_pred)
+
+def feature_importance(x_train):
+    feature_imp = pd.Series(clf.feature_importances_, index=x_train.columns).sort_values(ascending=False)
+    return feature_imp
+
+def open_file(filename):
+    with open(filename, 'rb') as f:
+        df = pickle.load(f)
+        return df
+
+def main():
+    x_train, y_train, x_test, y_test = data_cleaner.clean_data('wdbc.pkl', 0.7, df_as_numpy=False)
+    train_model(x_train,y_train)
+    y_pred = predict_model(x_test)
+    score = score_model(y_pred,y_test)
+    importance = feature_importance(x_train)
+
+    print(score,'\n', importance)
+
+
+if __name__ == '__main__':
+    main()
