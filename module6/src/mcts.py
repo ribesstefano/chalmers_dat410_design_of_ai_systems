4753eddebca3fce5e623cc992e470d418c192d73
danielemurgolo <98823551+danielemurgolo@users.noreply.github.com>
2022-02-28 22:41:12 +0100

    Update mcts.py

--- module6/src/mcts.py
+++ module6/src/mcts.py
@@ -92,8 +92,6 @@ class MonteCarloTreeSearchNode:
 
 
 def main():
-    game = TicTacToeGame(3)
-    game.reset()
     initial_state = game
     root = MonteCarloTreeSearchNode(state=initial_state)
     selected_node = root.best_action()
