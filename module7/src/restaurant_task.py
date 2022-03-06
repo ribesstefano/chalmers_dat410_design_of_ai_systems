6db1d3386e86173f9f9f0b4695b22e957a0314f6
danielemurgolo <98823551+danielemurgolo@users.noreply.github.com>
2022-03-07 00:23:05 +0100

    Update restaurant_task.py

--- module7/src/restaurant_task.py
+++ module7/src/restaurant_task.py
@@ -1,12 +1,32 @@
 from task import Task
 
+
 class RestaurantTask(Task):
     """docstring for RestaurantTask"""
+
     def __init__(self):
         super(RestaurantTask, self).__init__()
+        self.queries = {'location': None, 'type': None}
+        self.cusine_set = {'italian', 'indian', 'chinese'}
+        self.cities_set = {'Gothenburg', 'Malmo', 'Stockholm', 'Linkoping'}
+        self.times_set = {'today', 'tomorrow', 'afternoon', 'morning'}
 
-    def is_query_satisfied(self, query, sentence):
-        return False, ''
+    def are_queries_satisfied(self):
+        queries = self.queries.values()
+
+        for value in queries:
+            if value is None:
+                return False
+        return True
+
+    def get_queries(self, sentence):
+        for city in self.cities_set:
+            if city in sentence:
+                self.queries['location'] = city
+        for cusine in self.cusine_set:
+            if cusine in sentence:
+                print('im here')
+                self.queries['type'] = cusine
 
     def resolve_queries(self):
         """
@@ -16,4 +36,17 @@ class RestaurantTask(Task):
         :returns:   A reply to the user
         :rtype:     str
         """
+        while not self.are_queries_satisfied():
+            if self.queries.get('location') is None:
+                city = input('Please provide a location ')
+                if city not in self.cities_set:
+                    print('City not present in database')
+                else:
+                    self.queries['location'] = city
+            if self.queries.get('type') is None:
+                cusine = input('Please provide a type of cusine ')
+                if cusine not in self.cusine_set:
+                    print(f'Cusine inserted not valid please choose between :{self.cusine_set}')
+                else:
+                    self.queries['type'] = cusine
         return ''
