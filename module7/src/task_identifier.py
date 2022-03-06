50a59c7dd1ecd48c473ed7c75e2670a85f180186
Daniele29 <55363995+Daniele29@users.noreply.github.com>
2022-03-06 19:59:05 +0100

    task_identifier.py updated

--- module7/src/task_identifier.py
+++ module7/src/task_identifier.py
@@ -2,16 +2,38 @@ from task import Task
 from weather_task import WeatherTask
 from restaurant_task import RestaurantTask
 from tram_task import TramTask
+import spacy
+
+nlp = spacy.load('en_core_web_lg')
+
+weather_string = nlp('What is the weather forcast in the city?')
+restaurant_string = nlp('What is the closest Italian restaurant?')
+tram_string = nlp('When is the next tram from the station?')
+
+task_dic = {nlp(' '.join([str(t) for t in weather_string if t.pos_ in ['NOUN', 'PROPN']])): WeatherTask,
+            nlp(' '.join([str(t) for t in restaurant_string if t.pos_ in ['NOUN', 'PROPN']])): RestaurantTask,
+            nlp(' '.join([str(t) for t in tram_string if t.pos_ in ['NOUN', 'PROPN']])): TramTask}
+
 
 class TaskIdentifier(object):
     """docstring for TaskIdentifier"""
+
     def __init__(self):
         super(TaskIdentifier, self).__init__()
 
     def get_task_from_sentence(self, sentence):
         # Analyze sentence and return a specific task class
-        task = WeatherTask()
+        nlp_sentence = nlp(sentence)
+        no_verbs_sentence = nlp(' '.join([str(t) for t in nlp_sentence if t.pos_ in ['NOUN', 'PROPN']]))
+        task_sentences = list(task_dic.keys())
+        similarity_array = []
+        print(task_sentences)
 
-        if sentence.lower() == 'no':
-            return None
-        return task
+        for ref_sentence in task_sentences:
+            similarity_array.append(no_verbs_sentence.similarity(ref_sentence))
+
+        index = similarity_array.index(max(similarity_array))
+
+        print(task_sentences[index])
+
+        return task_dic[task_sentences[index]]
