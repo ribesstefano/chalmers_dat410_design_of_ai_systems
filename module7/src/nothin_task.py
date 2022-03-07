2f7fdca42796058c1bf0dd6aa83d194b612e632d
danielemurgolo <98823551+danielemurgolo@users.noreply.github.com>
2022-03-07 15:05:44 +0100

    Create nothin_task.py
    Task used when the computer doesn't understand what the user wants

---
+++ module7/src/nothin_task.py
@@ -0,0 +1,15 @@
+from task import Task
+
+
+class NothingTask(Task):
+    """docstring for WeatherTask"""
+
+    def __init__(self):
+        super(NothingTask, self).__init__()
+        self.queries = {'nothing': None}
+
+    def is_query_satisfied(self, query, sentence):
+        return True, 'I don\'t understand, please repeat'
+
+    def get_queries(self):
+        return self.queries.keys()
