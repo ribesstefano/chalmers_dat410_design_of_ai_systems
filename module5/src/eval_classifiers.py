91dc8f23642731f9789fde0b91444cb6e81d81c9
Stefano Ribes <stefanor@ribes.got.gaisler.com>
2022-03-08 16:41:00 +0100

    Module5: Updated classifiers comparison and plotting

--- module5/src/eval_classifiers.py
+++ module5/src/eval_classifiers.py
@@ -9,6 +9,45 @@ from sklearn.metrics import roc_curve, roc_auc_score
 import matplotlib.pyplot as plt
 import os
 
+def run_rule_based_model(dataset):
+    print('INFO. Starting Rule-based evaluation.')
+    x_train, y_train, x_test, y_test = dataset
+    rulebased = RuleBasedClassifier()
+    rulebased.fit(x_train, y_train)
+    y_scores = rulebased.predict(x_test)
+    auc = roc_auc_score(y_test, y_scores)
+    fpr, tpr, threshold = roc_curve(y_test, y_scores)
+    return auc, fpr, tpr
+
+def run_random_forest_model(dataset):
+    print('INFO. Starting Random Forest evaluation.')
+    x_train, y_train, x_test, y_test = dataset
+    NUM_ESTIMATORS = 100
+    clf = RandomForestClassifier(n_estimators=NUM_ESTIMATORS)
+    clf.fit(x_train, y_train)
+    y_scores = clf.predict_proba(x_test)
+    auc = roc_auc_score(y_test, y_scores[:, 1])
+    fpr, tpr, threshold = roc_curve(y_test, y_scores[:, 1])
+    return auc, fpr, tpr
+
+def run_knn_model(dataset):
+    print('INFO. Starting KNN evaluation.')
+    x_train, y_train, x_test, y_test = dataset
+    df, knn, knn_scaler = knn_model.get_max_auc(*dataset)
+    y_scores = knn_model.get_scores(knn, x_test, knn_scaler)
+    auc = roc_auc_score(y_test, y_scores[:, 1])
+    fpr, tpr, threshold = roc_curve(y_test, y_scores[:, 1])
+    return auc, fpr, tpr
+
+def run_svm_model(dataset):
+    print('INFO. Starting SVM evaluation.')
+    x_train, y_train, x_test, y_test = dataset
+    svm_auc, svm, svm_scaler = svm_model.get_max_auc(*dataset)
+    y_scores = svm_model.get_scores(svm, x_test, svm_scaler)
+    auc = roc_auc_score(y_test, y_scores)
+    fpr, tpr, threshold = roc_curve(y_test, y_scores)
+    return auc, fpr, tpr
+
 def main():
     # NOTE: Area Under the Receiver Operating Characteristic Curve (ROC AUC)
     # Clean and split the available data
@@ -17,34 +56,26 @@ def main():
     data_dir = os.path.abspath(data_dir)
     datafile = os.path.join(data_dir, 'wdbc.pkl')
     dataset = data_cleaner.clean_data(datafile, TRAIN_PERC, df_as_numpy=False)
-    x_train, y_train, x_test, y_test = dataset
-    # Random Forest
-    print('INFO. Starting Random Forest evaluation.')
-    NUM_ESTIMATORS = 100
-    clf = RandomForestClassifier(n_estimators=NUM_ESTIMATORS)
-    clf.fit(x_train, y_train)
-    y_scores = clf.predict_proba(x_test)
-    auc = roc_auc_score(y_test, y_scores[:, 1])
-    print(f'INFO. Random Forest ROC AUC: {auc:.4f}')
-    # KNN
-    print('INFO. Starting KNN evaluation.')
-    knn_auc, knn, knn_scaler = knn_model.get_max_auc(*dataset)
-    y_scores = knn_model.get_scores(knn, x_test, knn_scaler)
-    auc = roc_auc_score(y_test, y_scores[:, 1])
-    print(f'INFO. KNN ROC AUC: {auc:.4f}')
-    # SVC
-    print('INFO. Starting SVC evaluation.')
-    svm_auc, svm, svm_scaler = svm_model.get_max_auc(*dataset)
-    y_scores = svm_model.get_scores(svm, x_test, svm_scaler)
-    auc = roc_auc_score(y_test, y_scores)
-    print(f'INFO. SVC ROC AUC: {auc:.4f}')
-    # Rule-based
-    print('INFO. Starting Rule-based evaluation.')
-    rulebased = RuleBasedClassifier()
-    rulebased.fit(x_train, y_train)
-    y_scores = rulebased.predict(x_test)
-    auc = roc_auc_score(y_test, y_scores)
-    print(f'INFO. Rule-based ROC AUC: {auc:.4f}')
+    # Rule-based Classifier
+    auc, fpr, tpr = run_rule_based_model(dataset)
+    plt.plot(fpr, tpr, label=f'AUC={auc:0.2f} Rule-Based')
+    # Random Forest Classifier
+    auc, fpr, tpr = run_random_forest_model(dataset)
+    plt.plot(fpr, tpr, label=f'AUC={auc:0.2f} Random Forest')
+    # KNN Classifier
+    auc, fpr, tpr = run_knn_model(dataset)
+    plt.plot(fpr, tpr, label=f'AUC={auc:0.2f} KNN')
+    # SVC Classifier
+    auc, fpr, tpr = run_svm_model(dataset)
+    plt.plot(fpr, tpr, label=f'AUC={auc:0.2f} SVM')
+    # Final plotting
+    plt.plot([0, 1], [0, 1],'--') # Random classifier diagonal line
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc='lower right')
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.grid()
+    plt.show()
 
 
 if __name__ == '__main__':
